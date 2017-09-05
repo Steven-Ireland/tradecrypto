c21e8c1d4228b6acd61a691367f2e19f1965f0de
Kush <patel.kush.d@gmail.com>
2017-09-04 22:25:24 -0400

    Got the chatbot to connect and read messages
    Not sure why imports were dying but when I made the config a class and
    imported the class it worked. Found a better way to parge twitch
    messages with a compiled regex which just returns a string which made it
    smoother.
    
    I ran it and tested it by typing things in the channel chat and it was
    reading it !!!

--- chatbot/chatbot.py
+++ chatbot/chatbot.py
@@ -1,21 +1,26 @@
-import cfg
-import utils
+from cfg import Config
 import socket
-import thread
 import re
 
 def main():
+    str("starting")
     irc=socket.socket()
-    irc.connect((cfg.HOST, cfg.PORT))
-    irc.send('PASS {}\r\n'.format(cfg.PASS).encode("utf-8"))
-    irc.send('NICK {}\r\n'.format(cfg.PASS).encode("utf-8"))
-    irc.send('JOIN #{}\r\n'.format(cfg.PASS).encode("utf-8"))
+    irc.connect((Config.HOST, Config.PORT))
+    irc.send('PASS {}\r\n'.format(Config.PASS).encode("utf-8"))
+    irc.send('NICK {}\r\n'.format(Config.USER).encode("utf-8"))
+    irc.send('JOIN #{}\r\n'.format(Config.CHAN).encode("utf-8"))
+    print("Connected!")
+
+    CHAT_MSG = re.compile(r"^:\w+!\w+@\w+\.tmi\.twitch\.tv PRIVMSG #\w+ :")
 
     while True:
         readmsg = irc.recv(1024).decode("utf-8").rstrip()
-        if readmsg == "PING :tmi.twtich.tv\r\n":
-            irc.send("PONG :tmi.twtich.tv\r\n".encode("utf-8"))
-        else
-            user = re.search(r'(?<=\:)([0-9A-Za-z_]*?)(?=\!)', readmsg)
-            chan = re.search(r'(?<=PRIVMSG #)([0-9A-Za-z_]*?)(?= )', readmsg)
-            mesg = re.search(r'(?<= \:)([0-9A-Za-z ]*?)(?=$)', readmsg)
+        if readmsg == "PING :tmi.twitch.tv\r\n":
+            irc.send("PONG :tmi.twitch.tv\r\n".encode("utf-8"))
+        else:
+            user = re.search(r"\w+", readmsg).group(0)
+            msg = CHAT_MSG.sub("", readmsg)
+            print("User:" + user + " Message:" + msg)
+
+if __name__ == '__main__':
+    main()
